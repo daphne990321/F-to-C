addd8f31476df0516f796a53bdc8618c81152f6c
Daphne Huang <daphne990321@gmail.com>
2020-04-28 15:48:48 +0800

    introduction

---
+++ 123.py
@@ -0,0 +1,22 @@
+#布林值boolean bool(True,False)
+#字串 string str('Allen')
+
+#True or False
+x = 5
+#print(x == 5)
+#print(x !=5)
+#print(x > 2)
+#print(x < 2)
+#print(x <= 2)
+#it turns out booleen
+
+#age = input('請輸入年齡：')
+#age = int(age)
+#if age >= 20:
+#	print('你可以投票')
+
+F = input('請輸入攝氏溫度')
+F = float(F)
+C = (F-32)*9/5
+print('華氏溫度為', C)
+
